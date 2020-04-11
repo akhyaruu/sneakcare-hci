d8ec6773e4b313700035cbc06299491574894969
ilham akhyar <ilhamakhyar202@gmail.com>
2020-04-11 19:52:34 +0700

    perubahan file migrasi

--- database/migrations/2020_04_11_125113_create_orders_table.php
+++ database/migrations/2020_04_11_125113_create_orders_table.php
@@ -4,17 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePesanansTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    protected $table = 'pesanan';
     public function up()
     {
-        Schema::create('pesanans', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('nama_pemesan', 50);	
             $table->string('jenis_sepatu', 25);	
@@ -32,6 +31,6 @@ class CreatePesanansTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pesanans');
+        Schema::dropIfExists('orders');
     }
 }
