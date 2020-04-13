a84c87771b0303a00a4874a84792cb9310a68cea
ilham akhyar <ilhamakhyar202@gmail.com>
2020-04-13 22:10:43 +0700

    menambah register view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin_view/layout/main');
-});
+// Route::get('/', function () {
+//     return view('user_view/register_view');
+// });
+
+Route::post('/user/register', 'user\RegisterController@store');
+Route::get('/', 'user\RegisterController@index');
+
+
